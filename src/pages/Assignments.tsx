4b41e9d00b8bfb3b1f8eaccdfdd533247da7c032
surabhisingh12 <surabhis2004@gmail.com>
2025-04-30 22:26:15 -0400

    Added new functionalities to the frontend of webpage

--- src/pages/Assignments.tsx
+++ src/pages/Assignments.tsx
@@ -3,7 +3,7 @@
 function Assignments(){
   return (
     <div>
-      <h1>Assignments</h1>
+      <h3>Assignments</h3>
       <ul>
         <li>Homework 1 – Due Thursday</li>
         <li>Homework 2 – Due Next Monday</li>
